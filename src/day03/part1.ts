1d17c23b4a501b32de95cb40796a57786ed14eb2
selvalogesh <selvalogesh95@gmail.com>
2024-12-29 10:19:51 +0530

    Day 03 completed

--- src/day03/part1.ts
+++ src/day03/part1.ts
@@ -1,5 +1,14 @@
 // Advent of Code - Day 3 - Part One
 
 export function part1(input: string): number {
-  return 0;
+  let sum = 0;
+  const matched = input.matchAll(/mul\([0-9]+,[0-9]+\)/g);
+  for (const matchObj of matched) {
+    const [match] = matchObj
+    const [p1, p2] = match.split(",");
+    const n1 = parseInt(p1.slice(4, p1.length));
+    const n2 = parseInt(p2.slice(0, -1));
+    sum += n1 * n2;
+  }
+  return sum;
 }
--- src/day03/part2.ts
+++ src/day03/part2.ts
@@ -1,5 +1,27 @@
 // Advent of Code - Day 3 - Part Two
 
+function part1(input: string): number {
+  let sum = 0;
+  const matched = input.matchAll(/mul\([0-9]+,[0-9]+\)/g);
+  for (const matchObj of matched) {
+    const [match] = matchObj
+    const [p1, p2] = match.split(",");
+    const n1 = parseInt(p1.slice(4, p1.length));
+    const n2 = parseInt(p2.slice(0, -1));
+    sum += n1 * n2;
+  }
+  return sum;
+}
+
 export function part2(input: string): number {
-  return 0;
+  let sum = 0;
+  const matchedUntilInst = input.match(/mul\([0-9]+,[0-9]+\)((?!don't\(\)|do\(\)).)*/m);
+  const matchedDos = input.matchAll(/do\(\)((?!don't\(\)).|\n)*mul\([0-9]+,[0-9]+\)/g);
+  const [matchUt] = matchedUntilInst ?? [''];
+  sum += part1(matchUt);
+  for (const matchObj of matchedDos) {
+    const [match] = matchObj
+    sum += part1(match);
+  }
+  return sum;
 }
