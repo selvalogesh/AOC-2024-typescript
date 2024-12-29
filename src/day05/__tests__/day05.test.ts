cd7a5a8a5b081feb569c3d743c55efcf45c6fa6b
selvalogesh <selvalogesh95@gmail.com>
2024-12-29 14:52:37 +0530

    Day 05 start

---
+++ src/day05/__tests__/day05.test.ts
@@ -0,0 +1,15 @@
+// Advent of Code - Day 5
+
+import { describe, expect, test } from 'vitest';
+import { part1 } from '../part1.js';
+import { part2 } from '../part2.js';
+
+describe('Day 5', () => {
+  test('Part 1', () => {
+    expect(part1("")).toBe(0);
+  });
+
+  test('Part 2', () => {
+    expect(part2("")).toBe(0);
+  });
+});
