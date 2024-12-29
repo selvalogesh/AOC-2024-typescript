b4cc7b75aee401a2726f9145aacf54a6dadd9d3d
selvalogesh <selvalogesh95@gmail.com>
2024-12-29 14:31:13 +0530

    Day 04 completed

--- src/day04/part1.ts
+++ src/day04/part1.ts
@@ -1,5 +1,35 @@
 // Advent of Code - Day 4 - Part One
 
+function countVerticalAndDiagonal(input: Array<string>[]) {
+  let count = 0;
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      //vertical counting
+      if (input[i][j] === 'X' && input[i + 1]?.[j] === 'M' && input[i + 2]?.[j] === 'A' && input[i + 3]?.[j] === 'S') { count++; }
+      //diagonal counting right
+      if (input[i][j] === 'X' && input[i + 1]?.[j + 1] === 'M' && input[i + 2]?.[j + 2] === 'A' && input[i + 3]?.[j + 3] === 'S') { count++; }
+      //diagonal counting left
+      if (input[i][j] === 'X' && input[i + 1]?.[j - 1] === 'M' && input[i + 2]?.[j - 2] === 'A' && input[i + 3]?.[j - 3] === 'S') { count++; }
+    }
+  }
+  return count;
+}
+
 export function part1(input: string): number {
-  return 0;
+  let count = 0;
+  const horizontal = input.matchAll(/XMAS/g);
+  const horizontalRev = input.matchAll(/SAMX/g);
+  while (!horizontal.next().done) {
+    count++;
+  }
+  while (!horizontalRev.next().done) {
+    count++;
+  }
+  // input = input.replace(/XMAS|SAMX/g, "0000");
+  // vertical and diagonal
+  const twoDArr = input.split('\n').map(x => x.split(''));
+  count += countVerticalAndDiagonal(twoDArr);
+  twoDArr.reverse();
+  count += countVerticalAndDiagonal(twoDArr);
+  return count;
 }
--- src/day04/part2.ts
+++ src/day04/part2.ts
@@ -1,5 +1,24 @@
 // Advent of Code - Day 4 - Part Two
 
+function countVerticalAndDiagonal(input: Array<string>[]) {
+  let count = 0;
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 0; j < input[i].length; j++) {
+      let right = false;
+      let left = false;
+      //diagonal counting right
+      if (((input[i - 1]?.[j - 1] === 'M' && input[i + 1]?.[j + 1] === 'S') || (input[i - 1]?.[j - 1] === 'S' && input[i + 1]?.[j + 1] === 'M')) && input[i][j] === 'A') { right = true; }
+      //diagonal counting left
+      if (((input[i - 1]?.[j + 1] === 'M' && input[i + 1]?.[j - 1] === 'S') || (input[i - 1]?.[j + 1] === 'S' && input[i + 1]?.[j - 1] === 'M')) && input[i][j] === 'A') { left = true; }
+      if (right && left) count++;
+    }
+  }
+  return count;
+}
+
 export function part2(input: string): number {
-  return 0;
+  let count = 0;
+  const twoDArr = input.split('\n').map(x => x.split(''));
+  count += countVerticalAndDiagonal(twoDArr);
+  return count;
 }
